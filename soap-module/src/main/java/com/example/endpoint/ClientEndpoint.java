34cbfee449c76f35f35aca174da8e32688106fde
Kozubenko Dmitriy <kuzebenko@axiomatika.ru>
2023-11-15 11:15:45 +0300

    impl convert xml

--- soap-module/src/main/java/com/example/endpoint/ClientEndpoint.java
+++ soap-module/src/main/java/com/example/endpoint/ClientEndpoint.java
@@ -1,9 +1,34 @@
 package com.example.endpoint;
 
+import com.example.config.WebServiceConfig;
+import com.example.service.ConvertService;
+import com.soap_app.GetConvertedXmlRequest;
+import com.soap_app.GetConvertedXmlResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 @Endpoint
 public class ClientEndpoint {
 
+    private final ConvertService convertService;
 
+    @Autowired
+    public ClientEndpoint(ConvertService convertService) {
+        this.convertService = convertService;
+    }
+
+    @PayloadRoot(namespace = WebServiceConfig.NAMESPACE_URI, localPart = "convertedXmlRequest")
+    @ResponsePayload
+    public GetConvertedXmlResponse getClientInfo(@RequestPayload GetConvertedXmlRequest request) {
+        GetConvertedXmlResponse response = new GetConvertedXmlResponse();
+        try {
+            response.setConvertedXmlText(convertService.convertRequestToResponse(request.getSourceXmlText()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
 }
--- soap-module/src/main/java/com/example/service/ConvertService.java
+++ soap-module/src/main/java/com/example/service/ConvertService.java
@@ -1,9 +1,37 @@
 package com.example.service;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
 
 @Service
 public class ConvertService {
 
+    public String convertRequestToResponse(String requestXml) {
+        try {
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
 
+            Source xslt = new StreamSource(new ClassPathResource("convertRequestToResponse.xsl").getInputStream());
+
+            Transformer transformer = transformerFactory.newTransformer(xslt);
+
+            Source xsltSource = new StreamSource(new StringReader(requestXml));
+            StringWriter resultWriter = new StringWriter();
+            transformer.transform(xsltSource, new StreamResult(resultWriter));
+
+            return resultWriter.toString();
+        } catch (TransformerException | IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }

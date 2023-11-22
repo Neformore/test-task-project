85a7417bb12d7283ad8a6ed0d0b595e54f9db86c
Kozubenko Dmitriy <kuzebenko@axiomatika.ru>
2023-11-22 10:28:28 +0300

    start use @XmlAccessorType into dto

--- rest-module/src/main/java/com/example/dto/DocumentDto.java
+++ rest-module/src/main/java/com/example/dto/DocumentDto.java
@@ -3,58 +3,30 @@ package com.example.dto;
 import com.example.model.enums.DocumentType;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlAttribute;
 import jakarta.xml.bind.annotation.XmlRootElement;
+import lombok.Data;
 
-
+@Data
+@XmlAccessorType(XmlAccessType.FIELD)
 @XmlRootElement(name = "document")
 public class DocumentDto {
 
+    @XmlAttribute(name = "series")
     @Pattern(regexp = "[0-9]{4}", message = "Введена некорректная серия паспорта")
     private String series;
 
+    @XmlAttribute(name = "number")
     @Pattern(regexp = "[0-9]{6}", message = "Введен некорректный номер паспорта")
     private String number;
 
+    @XmlAttribute(name = "issueDate")
     @NotNull(message = "issueDate is null")
     private String issueDate;
 
+    @XmlAttribute(name = "type")
     @NotNull(message = "type is null")
     private DocumentType type;
-
-    @XmlAttribute(name = "series")
-    public String getSeries() {
-        return series;
-    }
-
-    public void setSeries(String series) {
-        this.series = series;
-    }
-
-    @XmlAttribute(name = "number")
-    public String getNumber() {
-        return number;
-    }
-
-    public void setNumber(String number) {
-        this.number = number;
-    }
-
-    @XmlAttribute(name = "issueDate")
-    public String getIssueDate() {
-        return issueDate;
-    }
-
-    public void setIssueDate(String issueDate) {
-        this.issueDate = issueDate;
-    }
-
-    @XmlAttribute(name = "type")
-    public DocumentType getType() {
-        return type;
-    }
-
-    public void setType(DocumentType type) {
-        this.type = type;
-    }
 }
--- rest-module/src/main/java/com/example/dto/PersonDto.java
+++ rest-module/src/main/java/com/example/dto/PersonDto.java
@@ -4,79 +4,34 @@ import com.example.model.enums.Gender;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.xml.bind.annotation.*;
+import lombok.Data;
 
+@Data
 @XmlRootElement(name = "person")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class PersonDto {
 
+    @XmlAttribute(name = "name")
     @Pattern(regexp = "[А-Я][а-я]+", message = "Введено некорректное имя")
     private String name;
 
+    @XmlAttribute(name = "surname")
     @Pattern(regexp = "[А-Я][а-я]+", message = "Введена некорректная фамилия")
     private String surname;
 
+    @XmlAttribute(name = "patronymic")
     @Pattern(regexp = "[А-Я][а-я]+", message = "Введено некорректное отчество")
     private String patronymic;
 
+    @XmlAttribute(name = "birthDate")
     @NotNull(message = "birthDate is null")
     private String birthDate;
 
+    @XmlAttribute(name = "gender")
     @NotNull(message = "gender is null")
     private Gender gender;
 
+    @XmlElement(name = "document")
     @NotNull(message = "document is null")
     private DocumentDto document;
-
-    @XmlAttribute(name = "name")
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @XmlAttribute(name = "surname")
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    @XmlAttribute(name = "patronymic")
-    public String getPatronymic() {
-        return patronymic;
-    }
-
-    public void setPatronymic(String patronymic) {
-        this.patronymic = patronymic;
-    }
-
-    @XmlAttribute(name = "birthDate")
-    public String getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(String birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    @XmlAttribute(name = "gender")
-    public Gender getGender() {
-        return gender;
-    }
-
-    public void setGender(Gender gender) {
-        this.gender = gender;
-    }
-
-    @XmlElement(name = "document")
-    public DocumentDto getDocument() {
-        return document;
-    }
-
-    public void setDocument(DocumentDto document) {
-        this.document = document;
-    }
 }
